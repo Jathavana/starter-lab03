5641668084234a71409752ceb9b380833f493f73
Jathavan-Mac <Jathavan-Mac@192.168.1.73>
2015-09-26 15:21:34 -0700

    Last Link Completed.

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->last();
+        $this->data = array_merge($this->data, $source);
+        
+        $this->render();
+    }
+
+}
+
+/* End of file Welcome.php */
